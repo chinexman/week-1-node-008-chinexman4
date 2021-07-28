298d715f3696d421530e650b3569a3701f487f7f
chinedu <chinedu.emordi@decagon.dev>
2021-07-28 15:04:46 +0100

    done and dusted

--- task-two/src/analysis.ts
+++ task-two/src/analysis.ts
@@ -1,77 +1,94 @@
+import fs from 'fs';
 
- const fs = require("fs");
-   
- let emails:string[] = [];
- let sampleJson:any = {};
- let validDomian:string[] = [];
+interface Domain {
+  'valid-domains': string[];
+  totalEmailsParsed: number;
+  totalValidEmails: number;
+  categories: {
+    [valid: string]: number;
+  };
+}
+
+const emails: string[] = [];
+const sampleJson: Domain = {};
+const validDomian: string[] = [];
 /**
  * First task - Read the csv files in the inputPath and analyse them
  *
  * @param {string[]} inputPaths An array of csv files to read
  * @param {string} outputPath The path to output the analysis
  */
-async function analyseFiles(inputPaths: string[], outputPath: string) {
-let result:any = await new Promise((resolve, reject)=>{
-fs.readFile(inputPaths[0],"utf-8",(err:string,data:string)=>{
-  if(err)
-  reject(err)
-  resolve(data)
-})
-})
-//console.log(result);
-let emailArray:string[]=[];
-let emailArray1:string[] = result.split("\n");
-  for( let elem = 0; elem < emailArray1.length; elem++){
-    console.log("index" + elem + " =" + emailArray1[elem]);
-     if(emailArray1[elem].includes("@")){
-       emailArray.push(emailArray1[elem]);
-     }
+
+function analyseFiles(inputPaths: string[], outputPath: string) {
+  let data = '';
+  const inputPaths1: string = inputPaths.join('');
+  try {
+    const readerStream = fs.createReadStream(inputPaths1);
+    readerStream.setEncoding('utf-8');
+
+    readerStream.on('data', function (chunk: string) {
+      data += chunk;
+    });
+
+    readerStream.on('end', function () {
+      //result = data;
+      console.log('it got here');
+      console.log(data);
+      console.log('it is working');
+
+      const emailArray: string[] = [];
+      const emailArray1: string[] = data.split('\n');
+      for (const elem = 0; elem < emailArray1.length; elem++) {
+        if (emailArray1[elem].includes('@')) {
+          emailArray.push(emailArray1[elem]);
+        }
+      }
+
+      for (const elem of emailArray) {
+        const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+        if (re.test(String(elem).toLowerCase())) {
+          //emailArr = elem + " ";
+          emails.push(elem);
+        }
+      }
+      //   type User = {
+
+      //     [key: string]: string;
+      // }
+      const obj: { [property: string]: number } = {};
+
+      for (let elem = 0; elem < emails.length; elem++) {
+        if (obj.prototype.hasOwnProperty.call(emails[elem].split('@')[1])) {
+          obj[emails[elem].split('@')[1]]++;
+        } else {
+          validDomian.push(emails[elem].split('@')[1]);
+          obj[emails[elem].split('@')[1]] = 1;
+        }
+      }
+
+      sampleJson['valid-domains'] = validDomian;
+      sampleJson['totalEmailsParsed'] = emailArray.length;
+      sampleJson['totalValidEmails'] = emails.length;
+      sampleJson['categories'] = obj;
+
+      console.log(sampleJson);
+
+      fs.writeFileSync(outputPath, JSON.stringify(sampleJson, null, ' '));
+    });
+  } catch (err) {
+    console.log(err);
   }
-  
-//console.log(emailArray)
-  for (let elem of emailArray){
-  //console.log(elem);
-    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-   
-   if(re.test(String(elem).toLowerCase())){
-    //emailArr = elem + " "; 
-    emails.push(elem)
-   }
-    
 
+  // let result:any = await new Promise((resolve, reject)=>{
+  // fs.readFile(inputPaths[0],"utf-8",(err:string,data:string)=>{
+  //   if(err)
+  //   reject(err)
+  //   resolve(data)
+  // })
+  // })
 
-  }
-//   type User = {
- 
-//     [key: string]: string;
-// }
-  let obj: any = {};
-
-for (let elem = 0; elem < emails.length; elem++){
-  if(obj.hasOwnProperty(emails[elem].split("@")[1])){
-
-  obj[emails[elem].split("@")[1]]++
-  }else{
-    validDomian.push(emails[elem].split("@")[1]);
-   obj[emails[elem].split("@")[1]]=1  
-  }
-}
-
-
-sampleJson["valid-domains"]=validDomian;
- sampleJson["totalEmailsParsed"]=emailArray.length;
- sampleJson["totalValidEmails"]=emails.length;
- sampleJson["categories"]=obj;
-
-//console.log(sampleJson);
-
-
-fs.writeFileSync(outputPath,JSON.stringify(sampleJson, null, " "));
-
-
-  //console.log('Complete the implementation in src/analysis.ts');
+  console.log('Complete the implementation in src/analysis.ts');
 }
 
 export default analyseFiles;
-
-//var mailformat = /^w+([.-]?w+)*@w+([.-]?w+)*(.w{2,3})+$/;
--- task-two/src/validation.ts
+++ task-two/src/validation.ts
@@ -1,6 +1,4 @@
-
-
-// import 
+// import
 // cons createCsvWriter = require('csv-writer').createObjectCsvWriter;
 // const csvWriter = createCsvWriter({
 //   path : 'sample-real.csv',
@@ -9,11 +7,11 @@
 //   ]
 // })
 
-// const records = 
-   const fs = require("fs");
-   
-   let emailArr:string = "";
-   let finalEmails:string[] = [];
+// const records =
+import fs from 'fs';
+
+const emailArr = '';
+const finalEmails: string[] = [];
 /**
  * Stretch goal - Validate all the emails in this files and output the report
  *
@@ -21,45 +19,58 @@
  * @param {string} outputFile The path where to output the report
  */
 
- async function validateEmailAddresses(inputPath: string[], outputFile: string) {
+async function validateEmailAddresses(inputPath: string[], outputFile: string) {
+  //   let result:any = await new Promise( (resolve,reject)=>{
 
-  let result:any = await new Promise( (resolve,reject)=>{
-    
-    fs.readFile(inputPath[0],"utf-8", (err :string, data:string)=>{
-    if(err) reject(err)
-      resolve(data)
+  //     fs.readFile(inputPath[0],"utf-8", (err :string, data:string)=>{
+  //     if(err) reject(err)
+  //       resolve(data)
 
-  })
-});
-//console.log(result);
+  //   })
+  // });
+  //console.log(result);
 
+  let data = ' ';
+  const inputPaths1: string = inputPath.join('');
+  try {
+    const readerStream = fs.createReadStream(inputPaths1);
+    readerStream.setEncoding('utf-8');
+
+    readerStream.on('data', function (chunk: string) {
+      data += chunk;
+    });
+
+    readerStream.on('end', function () {
+      //result = data;
+      console.log('it got here');
+      console.log(data);
+      console.log('it is working');
+
+      const emailArray: string[] = data.split('\n');
+
+      console.log(emailArray.length);
+      console.log('good');
+      for (const elem of emailArray) {
+        //console.log(elem);
+        const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+        if (re.test(String(elem).toLowerCase())) {
+          //emailArr = elem + " ";
+          finalEmails.push(elem);
+        }
+      }
+      console.log(finalEmails.length);
+      console.log(finalEmails);
+      console.log('it came here');
+      const csvEmail: string = finalEmails.join('');
+
+      fs.writeFileSync(outputFile, `${finalEmails}`);
+    });
+  } catch (err) {
+    console.log(err);
+  }
 
   //let emailArray = .split("\n");
-  let emailArray:string[] = result.split("\n");
- 
-  
-console.log(emailArray.length)
-console.log("good")
-  for (let elem of emailArray){
-  //console.log(elem);
-    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-   
-   if(re.test(String(elem).toLowerCase())){
-    //emailArr = elem + " "; 
-    finalEmails.push(elem)
-   }
-    
-
-
-  }
-console.log(finalEmails.length)
-  console.log(finalEmails);
-  let csvEmail:string = finalEmails.join("");
-
-
-  fs.writeFileSync(outputFile,`${finalEmails}`);
-
 }
 // validateEmailAddresses(["./index.ts"], "output.js")
-export default 
-validateEmailAddresses;
+export default validateEmailAddresses;
